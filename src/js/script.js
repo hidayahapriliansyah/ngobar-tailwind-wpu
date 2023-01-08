af432d71b486a7f8ef098a9d1e929790dbc091a4
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-08 19:03:27 +0700

    selesai cuy lupa belum dicomiit awokwok

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// Navba Fixed
+
+window.onscroll = () => {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+
+  window.pageYOffset > fixedNav
+  ? header.classList.add('navbar-fixed')
+  : header.classList.remove('navbar-fixed');
+};
+
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+hamburger.addEventListener('click', (e) => {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('nav-menu-active');
+});
